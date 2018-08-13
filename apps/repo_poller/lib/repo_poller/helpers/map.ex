0d4911b7745c3b93e564d009857e6df5e0afcaf1
sescobb27 <s.escob.b.27@gmail.com>
2018-08-13 16:59:23 -0500

    [add] map helpers to convert keys into existing atoms

---
+++ apps/repo_poller/lib/repo_poller/helpers/map.ex
@@ -0,0 +1,12 @@
+defmodule RepoPoller.Helpers.Map do
+  @spec safe_map_keys_to_atom(map()) :: map()
+  def safe_map_keys_to_atom(map) do
+    for {key, val} <- map, into: %{} do
+      if is_map(val) do
+        {String.to_existing_atom(key), safe_map_keys_to_atom(val)}
+      else
+        {String.to_existing_atom(key), val}
+      end
+    end
+  end
+end
