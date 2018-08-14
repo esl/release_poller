74d2f640033cb605bd3054eaa21271afa0b71411
sescobb27 <s.escob.b.27@gmail.com>
2018-08-14 13:37:03 -0500

    [add] dev config

--- apps/repo_poller/config/dev.exs
+++ apps/repo_poller/config/dev.exs
@@ -1,1 +1,7 @@
 use Mix.Config
+
+config :repo_poller, :repos, [
+  # {"https://github.com/elixir-lang/elixir", RepoPoller.Repository.Github, 60},
+  # {"https://github.com/inaka/erlang-katana", RepoPoller.Repository.Github, 60},
+  # {"https://github.com/inaka/erlang_guidelines", RepoPoller.Repository.Github, 60}
+]
