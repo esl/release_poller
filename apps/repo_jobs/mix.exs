d5a7402b4090eddd731749619fd30355893826d0
sescobb27 <s.escob.b.27@gmail.com>
2018-08-28 07:47:13 -0500

    [bump] upgrade and override httpoison version

--- apps/repo_jobs/mix.exs
+++ apps/repo_jobs/mix.exs
@@ -29,7 +29,7 @@ defmodule RepoJobs.MixProject do
       {:bugs_bunny, in_umbrella: true},
       {:domain, in_umbrella: true},
       {:poison, "~> 4.0"},
-      {:httpoison, "~> 0.8"}
+      {:httpoison, "~> 1.3"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
       # {:sibling_app_in_umbrella, in_umbrella: true},
--- apps/repo_poller/mix.exs
+++ apps/repo_poller/mix.exs
@@ -31,7 +31,8 @@ defmodule RepoPoller.MixProject do
       {:mock, "~> 0.3.2", only: :test},
       {:bugs_bunny, in_umbrella: true},
       {:domain, in_umbrella: true},
-      {:poison, "~> 4.0"}
+      {:poison, "~> 4.0"},
+      {:httpoison, "~> 1.3", override: true}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
       # {:sibling_app_in_umbrella, in_umbrella: true},
