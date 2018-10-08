af3ebba3a79b7b3e838feb978258c1af2d868aba
sescobb27 <s.escob.b.27@gmail.com>
2018-10-08 15:56:38 -0500

    [add] initial version of the docker builder integration
    [add] docker build integration
    [add] inject custom ENV to the build instructions
    [add] task now has a build_file path and a build_file_content which is the raw
    content of the docker build file
    [add] expand the raw content of the docker build file when creating a new task

--- apps/repo_jobs/mix.exs
+++ apps/repo_jobs/mix.exs
@@ -31,7 +31,7 @@ defmodule RepoJobs.MixProject do
       {:poison, "~> 4.0"},
       {:httpoison, "~> 1.3"},
       {:mox, "~> 0.4", only: :test},
-      {:ex_docker_build, "~> 0.1.0"}
+      {:ex_docker_build, "~> 0.3.0"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
       # {:sibling_app_in_umbrella, in_umbrella: true},
