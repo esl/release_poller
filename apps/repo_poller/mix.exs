673bb4b26d182445eff635a0aabc98094b919fe7
sescobb27 <s.escob.b.27@gmail.com>
2018-12-28 13:39:02 -0500

    [WIP] distributing repo_poller worker jobs across multiple nodes

--- apps/repo_poller/mix.exs
+++ apps/repo_poller/mix.exs
@@ -33,7 +33,8 @@ defmodule RepoPoller.MixProject do
       {:domain, in_umbrella: true},
       {:poison, "~> 4.0"},
       {:httpoison, "~> 1.3.0", override: true},
-      {:mox, "~> 0.4", only: :test}
+      {:mox, "~> 0.4", only: :test},
+      {:horde, git: "https://github.com/derekkraan/horde", branch: "master"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
       # {:sibling_app_in_umbrella, in_umbrella: true},
