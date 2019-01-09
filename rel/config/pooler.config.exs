808fd202e3f4b2789a184f14594e2ecd9d2d5e6c
sescobb27 <s.escob.b.27@gmail.com>
2019-01-09 13:03:49 -0500

    [fix] get rabbitmq host from the ENV [add] dockerignore file

--- rel/config/pooler.config.exs
+++ rel/config/pooler.config.exs
@@ -5,6 +5,7 @@ use Mix.Config
 config :repo_poller, :rabbitmq_config,
   channels: 10,
   queue: System.get_env("QUEUE_NAME"),
+  host: System.get_env("RABBIT_HOST"),
   exchange: "",
   queue_options: [durable: true],
   exchange_options: [durable: true]
