dcae46597ad28155056b88eaeef750d8efb6705a
sescobb27 <s.escob.b.27@gmail.com>
2019-01-21 08:41:29 -0500

    [change] error handling connecting con rabbitmq [add] docker inside docker

--- apps/bugs_bunny/lib/bugs_bunny/worker/setup_queue.ex
+++ apps/bugs_bunny/lib/bugs_bunny/worker/setup_queue.ex
@@ -12,12 +12,13 @@ defmodule BugsBunny.Worker.SetupQueue do
     queue_options = rabbitmq_config |> Keyword.get(:queue_options, [])
     exchange_options = rabbitmq_config |> Keyword.get(:exchange_options, [])
 
-    :ok =
-      BugsBunny.create_queue_with_bind(adapter, pool_id, queue, exchange, :direct,
-        queue_options: queue_options,
-        exchange_options: exchange_options
-      )
-
-    :ignore
+    BugsBunny.create_queue_with_bind(adapter, pool_id, queue, exchange, :direct,
+      queue_options: queue_options,
+      exchange_options: exchange_options
+    )
+    |> case do
+      :ok -> :ignore
+      {:error, _} = error -> {:stop, error}
+    end
   end
 end
--- jobs.dockerfile
+++ jobs.dockerfile
@@ -1,6 +1,17 @@
 # base image elixir to start with
 FROM bitwalker/alpine-elixir:1.7.4
 
+# install docker client
+RUN apk --update add curl \
+    && mkdir -p /tmp/download \
+    && curl -L "https://download.docker.com/linux/static/stable/x86_64/docker-18.09.1.tgz" | tar -xz -C /tmp/download \
+    && mv /tmp/download/docker/docker /usr/local/bin/ \
+    && rm -rf /tmp/download \
+    && apk del curl \
+    && rm -rf /var/cache/apk/*
+
+VOLUME /var/lib/docker
+
 # install hex package manager
 RUN mix local.hex --force
 
